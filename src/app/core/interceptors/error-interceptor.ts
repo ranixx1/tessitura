501aa3c1e12574303d1401cfaace1d83369911e1
ranilton <ranilton.c@escolar.ifrn.edu.br>
2026-08-08 17:54:30 -0300

    Add authInterceptor and errorInterceptor implementations

---
+++ src/app/core/interceptors/error-interceptor.ts
@@ -0,0 +1,23 @@
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
+
+export const errorInterceptor: HttpInterceptorFn = (req, next) => {
+  return next(req).pipe(
+    catchError((error: HttpErrorResponse) => {
+
+      if (error.status === 401) {
+        console.warn('Unauthorized request:', req.url);
+      }
+
+      if (error.status === 403) {
+        console.warn('Forbidden request:', req.url);
+      }
+
+      if (error.status >= 500) {
+        console.error('Server error:', error);
+      }
+
+      return throwError(() => error);
+    })
+  );
+};
