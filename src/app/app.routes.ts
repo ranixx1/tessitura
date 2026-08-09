26ab473a08710521b114f97c7becd5a645e1da1d
ranilton <ranilton.c@escolar.ifrn.edu.br>
2026-08-09 11:17:27 -0300

    feat: add ESLint configuration and enhance Angular environment setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./features/auth/pages/login/login')
+        .then(m => m.LoginComponent)
+  }
+];
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,19 @@
+import { CommonModule } from "@angular/common";
+import { CardComponent } from "../app/shared/components/card/card";
+import { InputComponent } from "../app/shared/components/input/input";
+import { ReactiveFormsModule } from "@angular/forms";
+import { RouterLink } from "@angular/router";
+import { ButtonComponent } from "../app/shared/components/button/button";
+
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080'
+  apiUrl: 'http://localhost:8080',
+  imports: [
+  CommonModule,
+  ReactiveFormsModule,
+  RouterLink,
+  ButtonComponent,
+  InputComponent,
+  CardComponent
+]
 };
