8b4867b6002776828c641d6b793b7432c2377f5b
ranilton <ranilton.c@escolar.ifrn.edu.br>
2026-08-09 11:35:46 -0300

    refactor: clean up environment configuration by removing unused imports and adjusting apiUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,19 +1,6 @@
-import { CommonModule } from "@angular/common";
-import { CardComponent } from "../app/shared/components/card/card";
-import { InputComponent } from "../app/shared/components/input/input";
-import { ReactiveFormsModule } from "@angular/forms";
-import { RouterLink } from "@angular/router";
-import { ButtonComponent } from "../app/shared/components/button/button";
-
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080',
-  imports: [
-  CommonModule,
-  ReactiveFormsModule,
-  RouterLink,
-  ButtonComponent,
-  InputComponent,
-  CardComponent
-]
+  apiUrl: 'http://localhost:8080/api/',
+  apiTimeout: 30000,
+  apiVersion: 'v1'
 };
