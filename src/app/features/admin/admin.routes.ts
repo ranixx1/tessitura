e96ca962090653dfa8d86307c784f16183edd46f
ranilton <ranilton.c@escolar.ifrn.edu.br>
2026-09-11 01:20:11 -0300

    feat(time-management): add time management feature with CRUD operations and UI

--- src/app/features/admin/admin.routes.ts
+++ src/app/features/admin/admin.routes.ts
@@ -1,12 +1,24 @@
 import { Routes } from '@angular/router';
+
 import { UsersManagementComponent } from './pages/users-management/users-management';
-import { roleGuard } from '../../core/guards/role-guard'; 
+import { TimeManagementComponent } from './pages/times/times-management';
+import { roleGuard } from '../../core/guards/role-guard';
 
 export const ADMIN_ROUTES: Routes = [
-    {
-        path: 'users',
-        component: UsersManagementComponent,
-        canActivate: [roleGuard],
-        data: { roles: ['ROLE_SUPERADMIN', 'ROLE_ADMIN'] }
+  {
+    path: 'users',
+    component: UsersManagementComponent,
+    canActivate: [roleGuard],
+    data: {
+      roles: ['ROLE_SUPERADMIN', 'ROLE_ADMIN'],
     },
+  },
+  {
+    path: 'times',
+    component: TimeManagementComponent,
+    canActivate: [roleGuard],
+    data: {
+      roles: ['ROLE_SUPERADMIN', 'ROLE_ADMIN'],
+    },
+  },
 ];
